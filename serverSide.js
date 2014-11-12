ae7f6cd817025d4ef82bf1e49147b3dc73902769
Andrew Bryson <andrew.bryson@capita.co.uk>
2014-11-12 17:08:06 +0000

    Adding basic auth

--- serverSide.js
+++ serverSide.js
@@ -18,7 +18,7 @@ context.on('ready', function () {
 			
 			db.newstuff.insert( { a: 1 }, function () {
 				console.log('SERVER, inserted new data');
-				var replyData = { now: new Date() };
+				var replyData = { now2: new Date() };
 				reply.write(JSON.stringify(replyData), 'utf8');
 			});
 			
