b3c3951a063d108d61758507a22cd792e10b6f1d
Andrew Bryson <Andrew Bryson>
2016-11-15 22:02:28 +0000

    perf testing

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
-var express = require('express');
-var http = require('http');
-var app = express.app = express();
-var server = http.createServer(app);
+// stock code from amqplib example, 1000 requests: 2 - 2.5 seconds
+// same with noDelay=true: similar requets
+// re-write code to create channel once and assert queue once: TBC ???
 
-var queueSocket = process.env.queueSocket || 'amqp://localhost';
-var messageName = 'app.message';
+var async = require('async');
+var q = 'tasks';
 
-var context = require('rabbit.js').createContext(queueSocket, function () {
-	console.log('context created');
-});
+function bail(err) {
+	console.error(err);
+	process.exit(1);
+}
 
-app.get('/', function (req, res) {
-	console.log('Request received');
-	var request = context.socket('REQ');
-		
-	request.connect(messageName, function () {
-		var data = { message: 'From client:' };
-		request.write(JSON.stringify(data), 'utf8');
+// Publisher
+function publisher(conn, callback) {
+	conn.createChannel(on_open);
+	function on_open(err, ch) {
+		if (err != null) bail(err);
+		ch.assertQueue(q);
+		ch.sendToQueue(q, new Buffer('something to do'));
+		callback(null, 'sent');
+	}
+}
+
+// Consumer
+function consumer(conn) {
+	var ok = conn.createChannel(on_open);
+	function on_open(err, ch) {
+		if (err != null) bail(err);
+		ch.assertQueue(q);
+		ch.consume(q, function (msg) {
+			if (msg !== null) {
+				// console.log('consumer: ' + msg.content.toString());
+				ch.ack(msg);
+			}
+		});
+	}
+}
+
+require('amqplib/callback_api')
+	.connect('amqp://localhost?noDelay=true', function (err, conn) {
+		if (err != null) bail(err);
+		consumer(conn);
+		// publisher(conn);
+
+		let numberOfRequests = 1000;
+		let requests = [];
+		function delegate() {
+			return function (callback) {
+				// console.log('calling consumer');
+				publisher(conn, callback);
+			}
+		}
+
+		for (let i = 0; i < numberOfRequests; i++) {
+			requests.push(delegate());
+		}
+
+		let start = new Date();
+		async.series(requests, () => {
+			console.log('all done');
+			let end = new Date() - start;
+			console.log('%d requests in %dms', numberOfRequests, (end));
+
+			process.exit(0);
+		})
 	});
-	
-	request.on('data', function (message) {
-		console.log('CLIENT, server said: ' + message);
-		res.write(message);
-		res.end();
-	});
-});
-
-var port = process.env.port || 3000;
-server.listen(port);
-console.log('Server listening on port: ' + port);
--- serverSide.js
+++ serverSide.js
@@ -1,5 +1,5 @@
-var mongojs = require('mongojs');
-var db = mongojs.connect('localhost:27017/bunny', [ 'newstuff' ]);
+// var mongojs = require('mongojs');
+// var db = mongojs.connect('localhost:27017/bunny', ['newstuff']);
 
 var queueSocket = process.env.queueSocket || 'amqp://localhost';
 var messageName = 'app.message';
@@ -11,17 +11,20 @@ var context = require('rabbit.js').createContext(queueSocket, function () {
 context.on('ready', function () {
 
 	var reply = context.socket('REP');
-	
+
 	reply.connect(messageName, function () {
 		reply.on('data', function (inMessage) {
 			console.log('SERVER, client said: ' + inMessage);
-			
-			db.newstuff.insert( { a: 1 }, function () {
-				console.log('SERVER, inserted new data');
-				var replyData = { sid: new Date() };
-				reply.write(JSON.stringify(replyData), 'utf8');
-			});
-			
+
+			var replyData = { sid: new Date() };
+			reply.write(JSON.stringify(replyData), 'utf8');
+
+			// db.newstuff.insert( { a: 1 }, function () {
+			// 	console.log('SERVER, inserted new data');
+			// 	var replyData = { sid: new Date() };
+			// 	reply.write(JSON.stringify(replyData), 'utf8');
+			// });
+
 		});
 	});
 
